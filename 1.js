3508010f038a96fee14e8964f23dffea4d0fea2c
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-10-23 15:21:39 +0900

    js

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
-for(let i = 0; i < 5; i++) {
-    for(let j = 0; j <= 1; j++) {
-        document.write('*')
-    }
-    document.write('br');
-}
+// for(let i = 0; i < 5; i++) {
+//     for(let j = 0; j <= 1; j++) {
+//         document.write('*')
+//     }
+//     document.write('br');
+// }
 
 // ctrl shift n을 누르면 코드가 실행된다.
+
+var now = new Date();
+    document.write(`<h1>${now}</h1>`);

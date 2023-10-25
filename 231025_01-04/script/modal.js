47fe111cd381c16705c2ca88e4b1bb476a6cef26
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-10-25 13:54:45 +0900

    ~4교시

---
+++ 231025_01-04/script/modal.js
@@ -0,0 +1,11 @@
+document.getElementById("modal_open").onclick = function() {
+    document.getElementById("modal_back").style.display = "block";
+};
+
+document.getElementById("modal_close").onclick = function() {
+    document.getElementById("modal_back").style.display = "none";
+};
+
+// $("#modal_open").click(function ()) {
+    // 
+// }

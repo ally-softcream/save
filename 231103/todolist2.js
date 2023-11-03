079b3a2945dbd51263310288a98b2a088201822c
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-11-03 17:21:12 +0900

    함수를 사용해 만든 배경색이 변하는 할일 목록

--- 231103/todolist2.js
+++ 231103/todolist2.js
@@ -14,9 +14,28 @@ function clickFunc() {
     const p = document.createElement("p");
     // createElement 함수 사용하면 만들 수 있다!
     // 4. 만든 p태그에 가져온 input value를 넣는다.
-    // 4.1 0~255 까지의 랜덤한 숫자 3개를 추출한다.
-    // 4.2 추출한 랜덤숫자로 p태그의 backgroundcolor를 바꿔준다.
     p.innerHTML = inputVal; // 하면 <p>값</p> 이런 상태가 된다
+    // 4.1 0~255 까지의 랜덤한 숫자 3개를 추출한다.
+    // const min = 0;
+    // const max = 255;
+    // const thrNumbers = 3;
+    let randomColors = [];
+    for (let i = 0; i < 3; i++) {
+        randomColors[i] = parseInt(Math.random() * 256);
+        // 
+        console.log(randomColors);
+    }
+    // 4.2 추출한 랜덤숫자로 p태그의 backgroundcolor를 바꿔준다.
+    p.style.backgroundColor = 
+    `rgb(${randomColors[0]}, ${randomColors[1]}, ${randomColors[2]})`;
+    // 4.3 글씨색도 바꿔준다.
+    const luma =
+    0.2126 * randomColors[0] + 0.7152 
+    * randomColors[1] + 0.0722 
+    * randomColors[2];
+    console.log(luma);
+    // 삼항 연산자 사용. 앞이 조건, 참:거짓. 참일경우 앞이고 거짓일경우 뒤 값이 나온다
+    p.style.color = parseInt(luma) <= 155 ? "white" : "black";
     // 5. div 요소를 가져온다.
     const div = document.querySelector("div");
     // 6. 가져온 h4 요소에 만든 p태그를 추가한다.

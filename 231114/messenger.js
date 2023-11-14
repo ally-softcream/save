4523f6103cc8cac479e6a5d6129b41e3400f54de
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-11-14 17:30:32 +0900

    메신저 최종

--- 231114/messenger.js
+++ 231114/messenger.js
@@ -30,6 +30,11 @@ const send = document.querySelector("#send");
 function sendMyText() {
     // 변수 선언
   const newMessage = input.value;
+  // 메세지 아무것도 입력 안 하면 엔터 안 쳐지게 만드는 코드
+  // 길이가 0이라면 함수가 종료하게 만드는 코드
+  if (newMessage.length == 0) {
+    return;
+  }
   // 새로운 요소 추가할 때는 createElement를 사용한다
   const div = document.createElement("div");
   // <div></div>
@@ -50,7 +55,18 @@ function sendMyText() {
 
 send.addEventListener("click", sendMyText);
 function sendMyTextByEnter(e) {
+    // 넘겨주는 정보 중에 key랑 shiftkey 라는 것이 있다
     console.log(e.key, e.shiftKey);
+    // enter 를 입력했을 때
+    // !는 부정연산자. 예: !true==false
+    if(e.key == "Enter" && !e.shiftKey) {
+        // 엔터키를 누르면 메세지를 보낸다
+        // 쉬프트키를 같이 누르면 메세지를 보내지 않는다
+        sendMyText();
+        // ↑ 메세지 보내는 함수 호출하기(또 다 적지 않아도 됨!)
+        e.preventDefault(); // ← 라는 새로운 함수 등장..
+        // 이 함수의 기능: 기본 이벤트를 취소한다
+    }
 }
 // 글자가 입력됐을 때 발생하는 이벤트 이름: keypress
 input.addEventListener("keypress", sendMyTextByEnter);

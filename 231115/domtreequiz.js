a9d0132e87c3ecf6d977ff36c77ea922cde452f8
Jiseon H <145522809+ally-softcream@users.noreply.github.com>
2023-11-15 16:59:49 +0900

    퀴즈 풀이 1

--- 231115/domtreequiz.js
+++ 231115/domtreequiz.js
@@ -1,5 +1,8 @@
 const bodyTag = document.querySelector("body");
-bodyTag.children[2].previousElementSibling.children[0].style.backgroundColor = "teal";
+bodyTag.children[2].previousElementSibling.children[0].style.backgroundColor = "lavender";
+bodyTag.firstElementChild.nextElementSibling.children[0].style.color = "tomato";
+bodyTag.children[1].firstElementChild.style.fontSize = "32px";
 
-// bodyTag.firstElementChild ==> red 로 색 바꿔보고
-// bodyTag.children[1] ==> blue 로 색 바꿔보기
+// bodyTag.firstElementChild ==> 를 사용해서 글씨색을 red 로 색 바꿔보고
+// bodyTag.children[1] ==> font 사이즈를 32px로 바꿔보기
+

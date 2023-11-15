7d63f30f94e7bd47f9b628b83c25f3cc88d14c34
Jiseon H <145522809+ally-softcream@users.noreply.github.com>
2023-11-15 16:30:13 +0900

    객체오브젝트 퀴즈 예제

---
+++ 231115/domtreequiz.js
@@ -0,0 +1,5 @@
+const bodyTag = document.querySelector("body");
+bodyTag.children[2].previousElementSibling.children[0].style.backgroundColor = "teal";
+
+// bodyTag.firstElementChild ==> red 로 색 바꿔보고
+// bodyTag.children[1] ==> blue 로 색 바꿔보기

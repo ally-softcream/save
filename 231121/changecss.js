a3b337b9d5ad703bb5f71f4d01b246574b95d60d
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-11-21 15:50:01 +0900

    제이쿼리 실습
    바탕색과 글씨색 바꿔보기 예제

---
+++ 231121/changecss.js
@@ -0,0 +1,8 @@
+// 전체 배경색을 변경
+$("*").css("background-color", "#E3BAC1");
+// h1요소의 글자색을 파랑으로 변경
+$("h2").css("color", "#6900EB");
+// id가 para인 요소의 글자색만을 파란색으로 변경
+$("#para").css("color", "#E600C1");
+// class가 sample인 요소의 글자색만을 빨강으로 변경
+$(".sample").css("color", "#E600C1");

a1ceb3ac043448880de216549f8b5856b851be05
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-11-23 17:29:57 +0900

    프롬프트로 내용 입력받아서 특정 요소에 반영하기

---
+++ 231123/var.js
@@ -0,0 +1,16 @@
+// // [aa=bb] ← 속성 선택자는 이런 문법으로 쓴다!
+// console.log($("label[for=user]").html());
+// console.log($("label[for=user]").text());
+
+// // 순수 자바스크립트
+// const userLabel = document.querySelector("label[for=user]");
+// // 이렇게 하면 화면에 있는 user(이름으로 표시)가 name으로 바뀐다!
+// userLabel.innerHTML = "Name";
+
+$("label[for=user]").html("Name");
+$("#user").val("이름");
+
+$("#btnChange").on("click", function () {
+    const newName = prompt("새로운 이름을 입력해주세요.");
+    $("#user").val(newName);
+  });

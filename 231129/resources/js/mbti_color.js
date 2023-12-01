64331048e6a69f6aed59be8bd2fc5721d197a6ba
ally-softcream <145522809+ally-softcream@users.noreply.github.com>
2023-12-01 17:29:22 +0900

    같은 파일 색상 입력하는대로 컬러칩 변경

--- 231129/resources/js/mbti_color.js
+++ 231129/resources/js/mbti_color.js
@@ -1,7 +1,20 @@
-"input".addEventListner("change",function(){
-    
-})
+// color input 값 가져오기
+const colorInput = document.querySelector(".color-input"); 
+console.log(colorInput);
+colorInput.addEventListener("change",function(){
+    // alert(colorInput.value);
+    const colorVal = colorInput.value;
+    if(colorVal.length == 7) {
+        const colorChip = document.querySelector(".color-input-chip");
+        // 가져온 colorchip의 값을 colorval의 값으로 바꿔주기
+        colorChip.style.backgroundColor = colorVal;
+    }
+});
 
-"input".on("change",function(){
-
-})
+// $(".color-input").on("change",function() {
+//     // alert($(this).val());
+//     const colorVal = $(this).val();
+//     if(colorVal.length == 7){
+//         $(".color-input-chip").css("backgroundColor", colorVal);
+//     }
+// });
